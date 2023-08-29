81e6bd20e7d4f62bb1ec27c61b69a66e20c4de66
tim-labahn <tim.labahn@freaknet.de>
2023-08-29 11:06:36 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <button class="btn btn-primary d-flex justify-content-center align-items-center">
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident rs voluptates, quae est deserunt error nulla dolore ut eius labore amet,
+    eligendi assumenda placeat. Dolorum, ex!
+  </button>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+</script>
+
+<style scoped></style>
